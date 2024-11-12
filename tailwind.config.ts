ee28751fb125ae4300f84a0bd0e809e922be9300
PaulHaze <dev@artemist.com.au>
2024-11-12 16:57:21 +1100

    add more custom tailwind rules

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,json}'],
@@ -71,5 +72,66 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities, matchUtilities, theme }) => {
+      addUtilities({
+        '.flex-center': {
+          display: 'flex',
+          'align-items': 'center',
+          'justify-content': 'center',
+        },
+        '.bb': {
+          borderWidth: '1px',
+          'border-color': 'rgb(220, 38, 38)',
+          '@media (min-width: 431px)': {
+            'border-color': '#64D267',
+          },
+          '@media (min-width: 640px)': {
+            'border-color': '#F5BD4F',
+          },
+          '@media (min-width: 768px)': {
+            'border-color': 'rgb(81, 142, 227)',
+          },
+          '@media (min-width: 1024px)': {
+            'border-color': 'rgb(220, 38, 38)',
+          },
+          '@media (min-width: 1280px)': {
+            'border-color': 'rgb(37, 99, 235)',
+          },
+        },
+        '.bb2': {
+          borderWidth: '1px',
+          'border-color': '#3b82f6',
+
+          '@media (min-width: 431px)': {
+            'border-color': '#d946ef',
+          },
+          '@media (min-width: 640px)': {
+            'border-color': '#84cc16',
+          },
+          '@media (min-width: 768px)': {
+            'border-color': '#ef4444',
+          },
+          '@media (min-width: 1024px)': {
+            'border-color': '#64748b',
+          },
+          '@media (min-width: 1280px)': {
+            'border-color': '#1e3a8a',
+          },
+        },
+      });
+      matchUtilities(
+        {
+          'animation-delay': (value) => {
+            return {
+              'animation-delay': value,
+            };
+          },
+        },
+        {
+          values: theme('transitionDelay'),
+        },
+      );
+    }),
+  ],
 } satisfies Config;
