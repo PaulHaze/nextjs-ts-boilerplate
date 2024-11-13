a329f8219f6226a9437eeccac38bd9275718839e
PaulHaze <dev@artemist.com.au>
2024-11-13 16:00:49 +1100

    add basic themeToggle svg button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from '@/components/ui';
+
 export default function Home() {
   return (
     <div className="relative">
-      {/* <div className="absolute right-2 top-3">
-      <ThemeToggle />
-    </div> */}
+      <div className="absolute right-2 top-3">
+        <ThemeToggle />
+      </div>
       <div className="flex flex-col items-center pt-10">
         <h1>TW/DaisyUI Next Starter</h1>
       </div>
