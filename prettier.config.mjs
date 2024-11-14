7aa52a393459cfdd46190d3b2c817c1963a6e148
PaulHaze <dev@artemist.com.au>
2024-11-14 16:31:49 +1100

    theme toggle cleanup

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -4,13 +4,13 @@
  */
 
 const config = {
-  // plugins: [require.resolve('prettier-plugin-tailwindcss')],
   semi: true,
   trailingComma: 'all',
   singleQuote: true,
   printWidth: 100,
   tabWidth: 2,
   plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['cn'],
 };
 
 export default config;
--- src/components/ui/ThemeToggler.tsx
+++ src/components/ui/ThemeToggler.tsx
@@ -1,7 +1,7 @@
 export function ThemeToggler() {
   return (
     <div className="">
-      <h1 className="child">ThemeToggler Placeholder</h1>
+      <h1 className="">ThemeToggler Placeholder</h1>
     </div>
   );
 }
