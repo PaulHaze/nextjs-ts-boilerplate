ae2aca8570ecb8c83ec30b4d0e2512580436abd4
PaulHaze <dev@artemist.com.au>
2024-11-20 17:23:15 +1100

    general clean up

--- src/components/ui/ThemeToggler.tsx
+++ src/components/ui/ThemeToggler.tsx
@@ -5,15 +5,10 @@ import { useTheme } from 'next-themes';
 export function ThemeToggler() {
   const { theme, setTheme } = useTheme();
   const toggleTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark');
+
   return (
     <div className="flex flex-col items-center gap-4">
-      <h1 className="">ThemeToggler</h1>
-      <button
-        onClick={toggleTheme}
-        aria-label="auto"
-        aria-live="polite"
-        className="theme-toggle pointer touch-manipulation rounded-full"
-      >
+      <button onClick={toggleTheme} aria-label="auto" aria-live="polite" className="theme-toggle">
         <svg className="sun-and-moon" aria-hidden="true" width="24" height="24" viewBox="0 0 24 24">
           {/* SUN */}
           <circle
@@ -42,58 +37,6 @@ export function ThemeToggler() {
           </mask>
         </svg>
       </button>
-      {/* <div className="h-20 w-20">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 46 46">
-          <path
-            fill="currentColor"
-            d="M18 6.42a1 1 0 0 0 1-1V1.91a1 1 0 0 0-2 0v3.51a1 1 0 0 0 1 1"
-            class="clr-i-solid clr-i-solid-path-1"
-          />
-          <path
-            fill="currentColor"
-            d="M18 29.58a1 1 0 0 0-1 1v3.51a1 1 0 0 0 2 0v-3.51a1 1 0 0 0-1-1"
-            class="clr-i-solid clr-i-solid-path-2"
-          />
-          <path
-            fill="currentColor"
-            d="M8.4 9.81A1 1 0 0 0 9.81 8.4L7.33 5.92a1 1 0 0 0-1.41 1.41Z"
-            class="clr-i-solid clr-i-solid-path-3"
-          />
-          <path
-            fill="currentColor"
-            d="M27.6 26.19a1 1 0 0 0-1.41 1.41l2.48 2.48a1 1 0 0 0 1.41-1.41Z"
-            class="clr-i-solid clr-i-solid-path-4"
-          />
-          <path
-            fill="currentColor"
-            d="M6.42 18a1 1 0 0 0-1-1H1.91a1 1 0 0 0 0 2h3.51a1 1 0 0 0 1-1"
-            class="clr-i-solid clr-i-solid-path-5"
-          />
-          <path
-            fill="currentColor"
-            d="M34.09 17h-3.51a1 1 0 0 0 0 2h3.51a1 1 0 0 0 0-2"
-            class="clr-i-solid clr-i-solid-path-6"
-          />
-          <path
-            fill="currentColor"
-            d="m8.4 26.19l-2.48 2.48a1 1 0 0 0 1.41 1.41l2.48-2.48a1 1 0 0 0-1.41-1.41"
-            class="clr-i-solid clr-i-solid-path-7"
-          />
-          <path
-            fill="currentColor"
-            d="m27.6 9.81l2.48-2.48a1 1 0 0 0-1.41-1.41L26.19 8.4a1 1 0 0 0 1.41 1.41"
-            class="clr-i-solid clr-i-solid-path-8"
-          />
-          <circle
-            cx="18"
-            cy="18"
-            r="10"
-            fill="currentColor"
-            class="clr-i-solid clr-i-solid-path-9"
-          />
-          <path fill="none" d="M0 0h36v36H0z" />
-        </svg>
-      </div> */}
     </div>
   );
 }
