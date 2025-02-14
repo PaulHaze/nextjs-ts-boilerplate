d5f78938ab53299da0559cd578e2a29922a2ee73
PaulHaze <dev@artemist.com.au>
2025-02-15 08:53:38 +1100

    main.css re-organized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center pt-10 px-2">
+    <div className="flex flex-col items-center pt-10 px-2 bg-background">
       <h1 className="font-semibold text-center">
         TW/DaisyUI/ShadCN Next Starter
       </h1>
