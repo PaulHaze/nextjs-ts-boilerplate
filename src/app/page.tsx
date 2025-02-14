7a34c48b5d81214c8d5904af12ac0ed9dbbe3035
PaulHaze <dev@artemist.com.au>
2025-02-15 10:16:29 +1100

    fixes to daisy theme colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center pt-10 px-2 bg-background">
-      <div className="size-20 bg-input"></div>
-      <h1 className="font-semibold text-center bg-input">
+    <div className="flex flex-col items-center pt-10 px-2 bg-base">
+      <h1 className="font-semibold text-center">
         TW/DaisyUI/ShadCN Next Starter
       </h1>
     </div>
