e621a8e9ec92d9bfb1bc83c6364b266d8ca14daa
PaulHaze <dev@artemist.com.au>
2025-02-15 16:51:45 +1100

    clean up page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center pt-10 px-2">
-      <h1 className="font-semibold text-center">
+    <div className="flex flex-col items-center px-2 pt-10">
+      <h1 className="text-center font-semibold">
         TW/DaisyUI/ShadCN Next Starter
       </h1>
-      <div className="mt-2 py-4 bg-black px-3"></div>
-      <div className="px-3 mt-2 bg-black py-4 mb-3"></div>
     </div>
   );
 }
